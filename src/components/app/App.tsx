4bb5f7fea0aa47970f0dde4f124f152526eaea6a
sadeghpro <sadeghmohamadnia@yahoo.com>
2022-05-29 15:26:03 +0430

    Add mui and rtl

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../home/Home'
 import Login from '../login/Login'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import RTL from './RTL';
+
 
 export default function App() {
+
+    const theme = createTheme({
+        direction: 'rtl',
+    });
+
     return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="login" element={<Login />} />
-            </Routes>
-        </>
+        <ThemeProvider theme={theme}>
+            <RTL rtl>
+                <CssBaseline />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                </Routes>
+            </RTL>
+        </ThemeProvider>
     )
 }
---
+++ src/components/app/RTL.jsx
@@ -0,0 +1,19 @@
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+export default function RTL({rtl, children}) {
+    if (rtl) {
+        document.dir = 'rtl';
+        return <CacheProvider value={cacheRtl}>{children}</CacheProvider>;
+    } else {
+        return children;
+    }
+}
