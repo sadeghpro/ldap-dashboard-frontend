07dc5ddb18e78f7e870c6534c94610cc9ca1da43
sadeghpro <sadeghmohamadnia@yahoo.com>
2022-05-29 14:49:13 +0430

    Add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import { BrowserRouter } from "react-router-dom";
+import './style.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
