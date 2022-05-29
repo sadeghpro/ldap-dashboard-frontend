8814647671a0892705c79c238a5958b1b75e3f1c
sadeghpro <sadeghmohamadnia@yahoo.com>
2022-05-29 14:41:39 +0430

    Add react router

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from '../home/Home'
+import Login from '../login/Login'
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+            </Routes>
+        </>
+    )
 }
