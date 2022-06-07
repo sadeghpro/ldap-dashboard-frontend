d9f91eab8aace8c1e039f5f7e87767b59fbc49b9
sadeghpro <sadeghmohamadnia@yahoo.com>
2022-06-07 12:49:08 +0430

    Add error dialog to the login page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,16 +1,22 @@
-import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
+import { Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography } from '@mui/material'
 import axios from 'axios';
 import React, { useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import IErrorResponse from '../../interfaces/IErrorResponse';
 import IResponse from '../../interfaces/IResponse';
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [dn, setDn] = useState('');
+    const [error, setError] = useState<IErrorResponse>({ code: 0, message: '' });
     const txtPassword = useRef<HTMLInputElement>();
     const { t } = useTranslation();
     const navigate = useNavigate();
+
+    const handleCloseDialog = () => {
+        setError({ code: 0, message: error.message });
+    }
 
     const handleChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value);
@@ -34,7 +40,7 @@ export default function Login() {
             if (res.data.status) {
                 navigate('/', { state: { data: res.data.data } });
             } else {
-                // TODO must show error message in dialog
+                setError(res.data.error!);
             }
         }).catch(err => {
 
@@ -45,11 +51,22 @@ export default function Login() {
         <Card className="center">
             <CardContent className="flex flex-col space-y-6 p-4">
                 <Typography className='text-center'>{t('app_name')}</Typography>
-                <TextField size="small" label={t('username')} fullWidth value={username} onChange={handleChangeUsername}/>
-                <TextField size="small" label={t('DN')} fullWidth value={dn} onChange={handleChangeDN}/>
-                <TextField size="small" label={t('password')} fullWidth inputRef={txtPassword} type="password"/>
+                <TextField size="small" label={t('username')} fullWidth value={username} onChange={handleChangeUsername} />
+                <TextField size="small" label={t('DN')} fullWidth value={dn} onChange={handleChangeDN} />
+                <TextField size="small" label={t('password')} fullWidth inputRef={txtPassword} type="password" />
                 <Button variant="contained" onClick={handleClick}>{t('signin')}</Button>
             </CardContent>
+            <Dialog open={error.code > 0} onClose={handleCloseDialog}>
+                <DialogTitle>{t('error_code')} {error.code}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {error.message}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog}>{t('close')}</Button>
+                </DialogActions>
+            </Dialog>
         </Card>
     )
 }
--- src/interfaces/IResponse.ts
+++ src/interfaces/IResponse.ts
@@ -1,4 +1,7 @@
+import IErrorResponse from "./IErrorResponse";
+
 export default interface IResponse<T> {
     status: boolean;
     data: T;
+    error?: IErrorResponse;
 }
